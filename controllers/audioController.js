f0dbdc32fafe07018616206547ed13129ca80376
makasinui <mks.shi@ya.ru>
2024-08-27 23:03:31 +0300

    tracks favourite

--- controllers/audioController.js
+++ controllers/audioController.js
@@ -1,9 +1,22 @@
-const { Track, Album, Artist } = require('../models/models');
+const { Track, Album, Artist, UserTrack } = require('../models/models');
+const jwt = require('jsonwebtoken')
 
 class AudioController {
     async getAll(req, res) {
+        const token = req.headers['authorization'];
+        let userId;
+        if(token) {
+            try {
+                const user = jwt.verify(token, process.env.SECRET_KEY);
+                userId = user.id
+            } catch(err) {
+                res.json({ message: err })
+            }
+        }
         try {
-            return res.json(await Track.findAll({ include: [Album, Artist] }));
+            return res.json(await Track.findAll({ 
+                include: [Album, Artist, {model: UserTrack, where: {userId}, required: false},],
+            }));
         } catch (err) {
             return res.json({ message: err });
         }
