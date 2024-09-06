08e5fb74c1d2663e77a309e427bbaa60064d6459
itdelta-prog <delta@it-delta.ru>
2024-09-06 11:24:20 +0300

    make favourite logic

--- routes/audioRouter.js
+++ routes/audioRouter.js
@@ -3,5 +3,6 @@ const router = new Router();
 const AudioController = require('../controllers/audioController');
 
 router.get('/', AudioController.getAll);
+router.post('/favourite', AudioController.makeFavourite);
 
 module.exports = router;
