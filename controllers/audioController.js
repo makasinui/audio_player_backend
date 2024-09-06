d964b5f71e9167429c3d99cc2205d71904bf7c95
makasinui <mks.shi@ya.ru>
2024-09-06 11:24:20 +0300

    make favourite logic

--- controllers/audioController.js
+++ controllers/audioController.js
@@ -12,13 +12,43 @@ class AudioController {
             } catch(err) {
                 res.json({ message: err })
             }
+            try {
+                return res.json(await Track.findAll({ 
+                    include: [Album, Artist, {model: UserTrack, where: {userId}, required: false},],
+                }));
+            } catch (err) {
+                return res.status(404).json({ message: err });
+            }
+        } else {
+            return res.json(await Track.findAll({include: [Album, Artist]}))
         }
-        try {
-            return res.json(await Track.findAll({ 
-                include: [Album, Artist, {model: UserTrack, where: {userId}, required: false},],
-            }));
-        } catch (err) {
-            return res.json({ message: err });
+    }
+
+    async makeFavourite(req, res) {
+        const token = req.headers['authorization'];
+        let userId;
+        
+        const { trackId, favourite } = req.body;
+        
+        if(token) {
+            try {
+                const user = jwt.verify(token, process.env.SECRET_KEY);
+                userId = user.id
+                if(favourite) {
+                    const user = await UserTrack.create({ userId, trackId })
+                    return res.json(user);
+                } else {
+                    const favouriteTrack = await UserTrack.findOne({where: { userId, trackId }});
+                    
+                    return res.json(await favouriteTrack.destroy());
+                }
+            } 
+            catch (err) {
+                return res.json({ message: err });
+            }
+
+        } else {
+            return res.status(401).json({ message: 'Unauthorized' });
         }
     }
 };
